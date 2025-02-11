6f182fb570a89816abdabc49f7faca2fbd7e500d
RenukaJsh <renukajoshi1412@gmail.com>
2025-02-11 15:51:33 +0530

    Update l.cpp

--- l.cpp
+++ l.cpp
@@ -1,7 +1,6 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-  cout<<"hello world";
-  return 0;
+include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
